be48d19bc105df7d9cebb9c3c93b108e739e360e
brent.fisher <brent.fisher@taulia.com>
2016-08-26 17:39:03 -0600

    In attempting to simulate races conditions, I get a cannot create concurrent execution exception.

---
+++ src/test/groovy/com/taulia/ppm/config/MySqlPaymentProcessEngineConfiguration.groovy
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2016. Taulia Inc. All rights reserved.
+ *
+ * All content of this file, its package and related information is to be treated
+ * as confidential, proprietary information.
+ *
+ * This notice does not imply restricted, unrestricted or public access to these materials
+ * which are a trade secret of Taulia Inc ('Taulia') and which may not be reproduced, used,
+ * sold or transferred to any third party without Taulia's prior written consent.
+ *
+ * Any rights not expressly granted herein are reserved by Taulia.
+ */
+
+package com.taulia.ppm.config
+
+import org.camunda.bpm.engine.spring.SpringProcessEngineConfiguration
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.io.ClassPathResource
+import org.springframework.core.io.Resource
+import org.springframework.jdbc.datasource.SimpleDriverDataSource
+
+import javax.sql.DataSource
+
+@Configuration
+class MySqlPaymentProcessEngineConfiguration extends TestPaymentProcessEngineConfiguration{
+
+  @Bean
+  @Override
+  public DataSource dataSource() {
+    SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+    dataSource.setDriverClass(com.mysql.jdbc.Driver.class);
+    dataSource.setUrl("jdbc:mysql://localhost:3306/taulia_payment_process_manager?useGmtMillisForDatetimes=true");
+    dataSource.setUsername("root");
+    dataSource.setPassword("");
+
+    return dataSource;
+  }
+
+  @Bean
+  @Override
+  SpringProcessEngineConfiguration processEngineConfiguration() {
+
+    SpringProcessEngineConfiguration springProcessEngineConfiguration = new SpringProcessEngineConfiguration()
+    springProcessEngineConfiguration.setDataSource(dataSource())
+    springProcessEngineConfiguration.setTransactionManager(transactionManager())
+    springProcessEngineConfiguration.setDatabaseSchemaUpdate('false')
+    springProcessEngineConfiguration.setJobExecutorActivate(false)
+    Resource[] resources = new Resource[1]
+    resources[0] = new ClassPathResource("payment-batch.bpmn")
+    springProcessEngineConfiguration.setDeploymentResources(resources)
+    springProcessEngineConfiguration
+  }
+
+
+}
--- src/test/groovy/com/taulia/ppm/services/ProcessEngineSpringTest.groovy
+++ src/test/groovy/com/taulia/ppm/services/ProcessEngineSpringTest.groovy
@@ -1,20 +1,18 @@
 package com.taulia.ppm.services
 
-import com.taulia.ppm.config.TestPaymentProcessEngineConfiguration
+import com.taulia.ppm.config.MySqlPaymentProcessEngineConfiguration
 import com.taulia.ppm.util.ProcessDiagramService
 import org.camunda.bpm.engine.HistoryService
 import org.camunda.bpm.engine.ManagementService
 import org.camunda.bpm.engine.RuntimeService
 import org.camunda.bpm.engine.TaskService
 import org.camunda.bpm.engine.history.HistoricVariableInstance
-import org.camunda.bpm.engine.management.JobDefinition
 import org.camunda.bpm.engine.runtime.Execution
+import org.camunda.bpm.engine.runtime.Job
 import org.camunda.bpm.engine.runtime.ProcessInstance
 import org.camunda.bpm.engine.runtime.VariableInstance
-import org.camunda.bpm.engine.runtime.VariableInstanceQuery
-import org.camunda.bpm.engine.task.Task
-import org.camunda.bpm.engine.test.Deployment
 import org.camunda.bpm.engine.test.ProcessEngineRule
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -25,11 +23,9 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 import static com.taulia.ppm.tasks.ProcessVariables.*
 import static com.taulia.ppm.tasks.ProcessDefinitions.*
 
-import static org.junit.Assert.assertEquals
-
 @RunWith(SpringJUnit4ClassRunner)
 @ContextConfiguration(classes = [
-  TestPaymentProcessEngineConfiguration,
+  MySqlPaymentProcessEngineConfiguration,
 ])
 class ProcessEngineSpringTest {
 
@@ -52,80 +48,47 @@ class ProcessEngineSpringTest {
   @Autowired
   ProcessDiagramService processDiagramService
 
+  @Before
+  public void stopProcesses() {
+    runtimeService.createProcessInstanceQuery().list().each { pi ->
+      runtimeService.deleteProcessInstance(pi.processInstanceId, "Starting new test run!")
+    }
+  }
+
   @Test
-  @Deployment(resources = ['payment-batch.bpmn'])
+//  @Deployment(resources = ['payment-batch.bpmn'])
   public void testBatches() {
-    String batchId = "batch-11"
-    println "Starting ${BATCH_PROCESS} with key:[${batchId}]"
-    ProcessInstance batchProcess = runtimeService.startProcessInstanceByKey(BATCH_PROCESS,
-      batchId,
-      [
-        (PAYMENT_BATCH_ID): batchId
-      ]
-    )
-    assert batchProcess
-    Execution receiveEPRTask = runtimeService.createExecutionQuery()
-      .activityId(RECEIVE_EPR_TASK)
-      .singleResult()
-
-
-    assert receiveEPRTask
+    ProcessInstance batchProcess = kickOffBatchProcess()
 
 
     int numEprs = 10
-    (1..numEprs).each { index ->
-      String earlyPaymentRequestId = "EPR-$index"
-
-      println "Starting EPR Process with key:[$earlyPaymentRequestId]"
-      ProcessInstance eprProcess = runtimeService.startProcessInstanceByKey(EPR_PROCESS,
-        earlyPaymentRequestId,
-        [
-          (EARLY_PAYMENT_REQUEST_ID): earlyPaymentRequestId
-        ]
-      )
-      println "EPR Process Instance ID:[${eprProcess.id}]"
+    double ratio = 0.50  // eprs to batch
+    kickOffEPRProcesses(numEprs,ratio)
 
 
-      assert eprProcess
+    checkVariablesPopulated(batchProcess, numEprs)
+
+
+    checkBackgroundJobsLeftToRun()
+  }
+
+  private void checkBackgroundJobsLeftToRun() {
+    println "Jobs left to run"
+    managementService.createJobQuery()
+      .list()
+      .each {
+      println it
+      processDiagramService.writeDiagramToFile(it.processInstanceId, "job:${it.processDefinitionId}.png")
     }
+  }
 
-    boolean batchProcessingIsFinished = false
-    while (!batchProcessingIsFinished) {
-      List<JobDefinition> jobDefinitions =
-        managementService.createJobQuery()
-          .list()
-      println "Found [${jobDefinitions?.size()}] job definition(s) to execute"
-
-      jobDefinitions.each {
-        if (!BATCH_PROCESS.equals(it.processDefinitionKey)) {
-          println "Executing Job:[${it.getProcessDefinitionId()}]"
-
-          managementService.executeJob(it.id)
-        } else {
-          println "Skipped job [$it]"
-        }
-      }
-
-
-      println "Finished executing [${jobDefinitions?.size()}] job definition(s)."
-
-      ProcessInstance pi = runtimeService.createProcessInstanceQuery()
-        .processInstanceBusinessKey(batchId)
-        .singleResult()
-
-      batchProcessingIsFinished = null != pi
-
-      println "Waiting on the batch processing to finish"
-      Thread.sleep(1000)
-    }
-
+  private void checkVariablesPopulated(ProcessInstance batchProcess, int numEprs) {
     println "Batch Process ID:[$batchProcess.id]"
     List<VariableInstance> variableInstances =
       runtimeService.createVariableInstanceQuery()
         .processInstanceIdIn(batchProcess.id)
         .list()
 
-    println "Batch Process Instance ID:[${batchProcess.id}]"
 
     variableInstances.each { variableInstance ->
       println "Variable:[${variableInstance?.name}:${variableInstance?.value}]"
@@ -145,13 +108,73 @@ class ProcessEngineSpringTest {
     assert batchProcessVariable
     assert numEprs == batchProcessVariable.value.size()
 
-    println "Jobs left to run"
-    managementService.createJobQuery()
-      .list()
-      .each {
-      println it
-      processDiagramService.writeDiagramToFile(it.processInstanceId,"job:${it.processDefinitionId}.png")
+  }
+
+  private void runBackgroundJobs() {
+    List<Job> jobDefinitions =
+      managementService.createJobQuery()
+        .list()
+    println "Found [${jobDefinitions?.size()}] job definition(s) to execute"
+
+    List<Job> sendEPRJobs = jobDefinitions.findAll { !BATCH_PROCESS.equals(it.processDefinitionKey)}
+    Job closeBatchJob = jobDefinitions.find { BATCH_PROCESS.equals(it.processDefinitionKey)} as Job
+    assert closeBatchJob
+
+    sendEPRJobs.eachWithIndex { jd,index ->
+      println "Executing Job:[${jd.getProcessDefinitionId()}]"
+
+      managementService.executeJob(jd.id)
     }
+    println "Kicking off another branch"
+    managementService.executeJob(closeBatchJob.id)
+  }
+
+
+  private List<Integer> kickOffEPRProcesses(int numEprs,double ratio) {
+    int numberOfEprsPerBatchMax = numEprs*ratio
+    int countOfEprsPerBatch = 0
+    (1..numEprs).each { index ->
+      String earlyPaymentRequestId = "EPR-$index"
+
+      println "Starting EPR Process with key:[$earlyPaymentRequestId]"
+      ProcessInstance eprProcess = runtimeService.startProcessInstanceByKey(EPR_PROCESS,
+        earlyPaymentRequestId,
+        [
+          (EARLY_PAYMENT_REQUEST_ID): earlyPaymentRequestId
+        ]
+      )
+      countOfEprsPerBatch ++
+      println "EPR Process Instance ID:[${eprProcess.id}]"
+      if(countOfEprsPerBatch>=numberOfEprsPerBatchMax)
+      {
+        countOfEprsPerBatch = 0
+        runBackgroundJobs()
+      }
+
+      assert eprProcess
+
+    }
+  }
+
+  private ProcessInstance kickOffBatchProcess() {
+    String batchId = "batch-11"
+    println "Starting ${BATCH_PROCESS} with key:[${batchId}]"
+    ProcessInstance batchProcess = runtimeService.startProcessInstanceByKey(BATCH_PROCESS,
+      batchId,
+      [
+        (PAYMENT_BATCH_ID): batchId
+      ]
+    )
+    assert batchProcess
+
+    Execution receiveEPRTask = runtimeService.createExecutionQuery()
+      .processInstanceId(batchProcess.processInstanceId)
+      .activityId(RECEIVE_EPR_TASK)
+      .singleResult()
+
+
+    assert receiveEPRTask
+    batchProcess
   }
 
 }
